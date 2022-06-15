25265e92540d1ad315c5a4d92ad8909d85f409b4
Jocelin21 <jocelin.wilson@binus.ac.id>
2022-06-16 02:19:43 +0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include <array>
+#include <queue>
 
 using namespace std;
 
@@ -58,49 +59,86 @@ void addList(){
 }
 
 int main(){
+    queue<string> queueItem;
+    queue<int> queueData;
+    string item;
     int opt1;
     bool check = true;
 
     cout << "Welcome to List" << endl;
-    cout << "What would you like to do?" << endl;
 
-    while (true) {
+    while (check) {
+        cout << "What would you like to do?" << endl;
         cout << "1. Add Item" << endl;
         cout << "2. Add Data" << endl;
         cout << "3. Delete Item" << endl;
         cout << "4. Delete Data" << endl;
-        cout << "5. Display Data" << endl;
+        cout << "5. Display Item" << endl;
+        cout << "6. Display Data" << endl;
+        cout << "7. Exit" << endl;
         cout << "Enter your input : ";
         cin >> opt1;
 
-        while (check){
-            switch (opt1){
-            case 1:
-                cout << "============================================" << endl;
-                break;
+        switch (opt1){
+        case 1:
+            cout << "============================================" << endl;
+            cout << "Enter the Item: ";
+            cin >> item;
+            queueItem.push(item);
+            cout<< "'" << item << "' has successfuly added!" << endl;
+            break;
 
-            case 2:
-                cout << "============================================" << endl;
-                break;
+        case 2:
+            cout << "============================================" << endl;
+            cout << "Enter the Data: ";
+            int data;
+            cin >> data;
+            queueData.push(data);
+            cout<< "'" << data << "' has successfuly added!" << endl;
+            break;
 
-            case 3:
-                cout << "============================================" << endl;
-                break;
-            
-            case 4:
-                cout << "============================================" << endl;
-                break;
+        case 3:
+            cout << "============================================" << endl;
+            cout<< "'" << queueItem.front() << "' succefully deleted!" << endl;
+            queueItem.pop();
+            break;
+        
+        case 4:
+            cout << "============================================" << endl;
+            cout<< "'" << queueData.front() << "' succefully deleted!" << endl;
+            queueData.pop();
+            break;
 
-            case 5:
-                cout << "============================================" << endl;
-                break;
-            
-            default:
-                cout << "============================================" << endl;
-                cout << "Sorry, your input is not valid !!" << endl;
-                cout << "Please re-enter a number in range 1-5 !!" << endl;
-                break;
+        case 5:
+            cout << "============================================" << endl;
+            cout<< "[";
+            while(!queueItem.empty()){
+                cout<< queueItem.front() << " ";
+                queueItem.pop();
             }
+            cout<< "]" << endl;
+            break;
+
+        case 6:
+            cout << "============================================" << endl;
+            cout<< "[";
+            while(!queueData.empty()){
+                cout<< queueData.front() << " ";
+                queueData.pop();
+            }
+            cout<< "]" << endl;
+            break;
+
+        case 7:
+            cout << "============================================" << endl;
+            cout << "Exiting..." << endl;
+            check = false;
+            break;
+        
+        default:
+            cout << "============================================" << endl;
+            cout << "Sorry, your input is not valid !!" << endl;
+            cout << "Please re-enter a number in range 1-5 !!" << endl;
             break;
         }
     }
